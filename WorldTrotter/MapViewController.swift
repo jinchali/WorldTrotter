02f8364b8d4d133bd1a54897565654c985975a4f
Jackson Inchalik <jinchalik@wolverine.highpoint.edu>
2017-02-05 22:32:28 -0500

    Chapter 5 completed without silver challenge

---
+++ WorldTrotter/MapViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by Jackson Inchalik on 2/5/17.
+//  Copyright © 2017 Jackson Inchalik. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("MapViewController loaded its view.")
+    }
+    
+}
